ca0b1b82a161889af920065536374d8d22e31569
dlopoelinreverse <l.lesaul@gmail.com>
2023-08-07 19:47:10 +0200

    header translations

--- components/header/PageTitle.tsx
+++ components/header/PageTitle.tsx
@@ -4,5 +4,8 @@ import { useSelectedLayoutSegment } from "next/navigation";
 
 export default function PageTitle() {
   const activeSegment = useSelectedLayoutSegment();
+  const translation = {
+    applies: "candidatures",
+  };
   return <h3>{activeSegment}</h3>;
 }
